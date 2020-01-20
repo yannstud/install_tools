34bc1e2003526bd85ac172001a60d2a94c47cd28
yannstud <ysalaun@student.42.fr>
2020-01-20 09:47:16 +0100

    some added

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-## Perso Installation UBUNTU
+# Personnal UBUNTU Installation 
 
 Script need to be run as sudo 
 
-# Installation by hand needed 
+## Installation by hand needed 
 
 Burp Suite:
 	http://www.bitforestinfo.com/2017/08/how-to-install-burp-suite-in.html
--- install.sh
+++ install.sh
@@ -1,26 +1,54 @@
 #!/bin/bash
 
-# install tools 
+GREEN="\e[32m"
+WHITE="\e[39m"
+
+
+# Basic tools
+echo "$GREEN installing vim $WHITE"
 sudo apt install vim
+echo "$GREEN installing git $WHITE"
 sudo apt install git
+echo "$GREEN installing curl $WHITE"
 sudo apt install curl
+echo "$GREEN installing zsh $WHITE"
 sudo apt install zsh
-# install window tilling manager i3
+
+# window tilling manager i3
 sudo apt install i3 
 
-# install oh-my-zsh
+# oh-my-zsh
+echo "$GREEN installing oh-my-zsh $WHITE"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# install sublime text 
+# sublime text 
+echo "$GREEN installing Sublime text $WHITE"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text
 
-# install terminator 
+# terminator 
+echo "$GREEN installing Terminator $WHITE"
 sudo add-apt-repository ppa:gnome-terminator
 sudo apt-get update
 sudo apt-get install terminator
 
-# install pentesting tools
+# virtualisation tools
+echo "$GREEN installing virt-manager $WHITE"
+sudo apt install virt-manager 
 
+# pentesting tools
+echo "$GREEN installing dirb $WHITE"
+sudo apt install dirb
+echo "$GREEN installing wfuzz $WHITE"
+sudo apt install wfuzz
+echo "$GREEN installing nmap $WHITE"
+sudo apt install nmap
+echo "$GREEN installing openvpn $WHITE"
+sudo apt install openvpn
+echo "$GREEN installing john-the-ripper $WHITE"
+sudo snap install john-the-ripper
+
+# aliases 
+echo 'alias vpn="sudo openvpn ~/HackTheBox/floky.ovpn"' >> ~/.zshrc
